06f53a02b3368be394db12287c1038ae40a4452b
KhweziOfentse <158312796+KhweziOfentse@users.noreply.github.com>
2024-05-22 19:50:39 +0200

    add api to search engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector(".current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "b3at1f54094o5ce1cac43e10abb70bf3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function search(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector(".search-input");
-  let cityElement = document.querySelector(".current-city");
-  cityElement.innerHTML = searchInputElement.value;
+  let searchInput = document.querySelector(".search-input");
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
+
+searchCity("Pretoria");
