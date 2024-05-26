d9547a6dda31a8addc595ce247cc2e221a791cca
KhweziOfentse <158312796+KhweziOfentse@users.noreply.github.com>
2024-05-26 17:41:18 +0200

    API integration

--- src/index.js
+++ src/index.js
@@ -60,6 +60,13 @@ function formatDate(date) {
   return day + " " + hours + ":" + minutes;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "b3at1f54094o5ce1cac43e10abb70bf3";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -67,26 +74,27 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="forecast-date">
-        <div class="forecast-day">${day}</div>
-        <div class="forecast-icon">🌤️</div>
+        <div class="forecast-day">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="forecast-icon" />
         <div class="forecast-temperatures">
           <div class="forecast-max">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
           </div>
-          <div class="forecast-min">9º</div>
+          <div class="forecast-min">${Math.round(
+            day.temperature.minimum
+          )}º</div>
         </div>
       </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
